8a6af00cdbcd16f79b24c41fa297bb3c5bb8d831
NayRSilva <nayarasilva@cjr.org.br>
2019-06-29 14:21:29 -0300

    primeiro commit

---
+++ app/models/product.rb
@@ -0,0 +1,13 @@
+class Product < ApplicationRecord
+    self.inheritance_column = :type
+
+    def self.type
+        %w(Game Console)
+    end
+
+    scope :games, -> { where(type: 'Game') } 
+scope :consoles, -> { where(type: 'Console') }
+end
+
+class Game < Product; end
+class Console < Product; end
